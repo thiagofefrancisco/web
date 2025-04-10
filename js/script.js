b8d3fc7b5d9c3ddec3e4f924740518a7cec251be
Thiago Francisco | Web Designer <thiagofefrancisco@gmail.com>
2025-04-09 21:26:27 -0300

    Update h1 h2

--- js/script.js
+++ js/script.js
@@ -5,6 +5,10 @@ const rootHtml = document.documentElement;
 function changeTheme () {
     const currentTheme = rootHtml.getAttribute("data-theme");
     currentTheme === "dark" ? rootHtml.setAttribute("data-theme","light") : rootHtml.setAttribute("data-theme", "dark");
+
+    toggleTheme.classList.toggle("bi-sun");
+    toggleTheme.classList.toggle("bi-moon-stars");
+
 }
 
 toggleTheme.addEventListener("click", changeTheme);
