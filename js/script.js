69db063ea26f8a868a9e9afdd66a70b95a403681
Thiago Francisco | Web Designer <thiagofefrancisco@gmail.com>
2025-04-25 21:27:46 -0300

    Update Form

--- js/script.js
+++ js/script.js
@@ -1,7 +1,10 @@
 /* Const é igual ao Objeto Ícone */
 const toggleTheme = document.getElementById("toggleTheme");
 const rootHtml = document.documentElement;
+const accordionHeader = document.querySelectorAll(".accordion-header");
+const menuLinks = document.querySelectorAll(".menu-link");
 
+/* Function Trocar Tema */
 function changeTheme () {
     const currentTheme = rootHtml.getAttribute("data-theme");
     currentTheme === "dark" ? rootHtml.setAttribute("data-theme","light") : rootHtml.setAttribute("data-theme", "dark");
@@ -14,9 +17,6 @@ function changeTheme () {
 toggleTheme.addEventListener("click", changeTheme);
 
 /* Function Accordion */
-
-const accordionHeader = document.querySelectorAll(".accordion-header");
-
 accordionHeader.forEach(header => {
     header.addEventListener("click", () => {
 
@@ -24,5 +24,13 @@ accordionHeader.forEach(header => {
         const accordionActive = accordionItem.classList.contains("active");
         
         accordionActive ? accordionItem.classList.remove("active") : accordionItem.classList.add("active");
+    })
+})
+
+/* Deixar o item do menu ativo */
+menuLinks.forEach(item => {
+    item.addEventListener("click", () => {
+        menuLinks.forEach(i => i.classList.remove("active"));
+        item.classList.add("active");
     })
 })
